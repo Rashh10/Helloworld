f61e297ee2d997cc8bffde752b826cc3a812a9d6
Kaajal20 <90800242+Kaajal20@users.noreply.github.com>
2021-10-19 10:36:52 +0530

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,6 +1,6 @@
 //Hello, World program in Java
  class FirstProgram{
      public static void main(String[] args){
-         System.out.println("Hy, This is Rashmi.");
-         }
+         System.out.println("Hello, This is Rashmi.");
+     }
  }
